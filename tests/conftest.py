28b63ec73e4d7a23af58156076262658e7d92015
Varlachev Valery <valvarl@ya.ru>
2025-03-19 23:16:08 +0300

    two-square rule tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,9 @@
 import pytest
 
-from stratego_gym.envs.config import StrategoConfig, GameMode
+from stratego_gym.envs.config import (
+    StrategoConfig, GameMode, 
+    PLACES_TO_DEPLOY_RED_ORIGINAL, PLACES_TO_DEPLOY_BLUE_ORIGINAL, LAKES_ORIGINAL
+)
 from stratego_gym.envs.primitives import Piece
 from stratego_gym.envs.startego import StrategoEnv
 
@@ -31,3 +34,19 @@ def env_5x5():
         env.reset()
         return env
     return _env_5x5
+
+@pytest.fixture()
+def env_10x10():
+    def _env_10x10(pieces_num: dict[Piece, int], lakes=LAKES_ORIGINAL) -> StrategoEnv:
+        config = StrategoConfig(
+            height=10,
+            width=10,
+            p1_pieces_num=pieces_num,
+            p1_places_to_deploy=PLACES_TO_DEPLOY_RED_ORIGINAL,
+            p2_places_to_deploy=PLACES_TO_DEPLOY_BLUE_ORIGINAL,
+            lakes=lakes,
+        )
+        env = StrategoEnv(config)
+        env.reset()
+        return env
+    return _env_10x10
--- tests/env/test_two_square_rule.py
+++ tests/env/test_two_square_rule.py
@@ -1,11 +1,12 @@
 
 import itertools
+from typing import Any, Callable
 import pytest
 
 import numpy as np
 
 from stratego_gym.envs.primitives import Piece, Player
-from stratego_gym.envs.startego import GamePhase
+from stratego_gym.envs.startego import GamePhase, StrategoEnv
 
 SCOUT_ONLY = {
     Piece.SCOUT: 1,
@@ -61,7 +62,7 @@ def validate_move(env, piece, from_pos, to_pos, maybe_occupied=False):
         itertools.product(range(3, 5), range(5)),
     )
 )
-def test_one_step_move(env_5x5, pices, from_pos):
+def test_one_step_move(env_5x5: Callable[[Any], StrategoEnv], pices, from_pos):
     env = env_5x5(pices)
     move_direction1 = np.array([-1, 1, 0, 0])
     move_direction2 = np.roll(move_direction1, 2)
@@ -104,7 +105,7 @@ def test_one_step_move(env_5x5, pices, from_pos):
         itertools.product(range(3, 5), range(5)),
     )
 )
-def test_no_violation_three_steps(env_5x5, pices, from_pos):
+def test_no_violation_three_steps(env_5x5: Callable[[Any], StrategoEnv], pices, from_pos):
     env = env_5x5(pices)
     move_direction1 = np.array([-1, 1, 0, 0])
     move_direction2 = np.roll(move_direction1, 2)
@@ -152,7 +153,7 @@ def test_no_violation_three_steps(env_5x5, pices, from_pos):
         itertools.product(range(3, 5), range(5)),
     )
 )
-def test_no_violation_square_movement(env_5x5, pices, from_pos):
+def test_no_violation_square_movement(env_5x5: Callable[[Any], StrategoEnv], pices, from_pos):
     env = env_5x5(pices)
     move_direction1 = np.array(2 * [-1, 0, 1, 0])
     move_direction2 = np.roll(move_direction1, 1)
@@ -187,7 +188,7 @@ def test_no_violation_square_movement(env_5x5, pices, from_pos):
         itertools.product(range(3, 5), range(1, 4)),
     )
 )
-def test_no_violation_interrupt(env_5x5, pices, from_pos):
+def test_no_violation_interrupt(env_5x5: Callable[[Any], StrategoEnv], pices, from_pos):
     env = env_5x5(pices)
     move_direction1 = np.array([0, 0])
     move_direction2 = np.array([-1, 1])
@@ -245,7 +246,7 @@ def test_no_violation_interrupt(env_5x5, pices, from_pos):
         itertools.product(range(3, 5), range(1, 4)),
     )
 )
-def test_no_violation_interrupt_sync(env_5x5, pices, from_pos):
+def test_no_violation_interrupt_sync(env_5x5: Callable[[Any], StrategoEnv], pices, from_pos):
     env = env_5x5(pices)
     move_direction1 = np.array([-1, 1, 0, 0])
     move_direction2 = np.roll(move_direction1, 2)
@@ -297,7 +298,7 @@ def test_no_violation_interrupt_sync(env_5x5, pices, from_pos):
         itertools.product(range(3, 5), range(5)),
     )
 )
-def test_scout_move_entire_row(env_5x5, pices, from_pos):
+def test_scout_move_entire_row(env_5x5: Callable[[Any], StrategoEnv], pices, from_pos):
     env = env_5x5(pices)
     move_direction1 = np.array([-1, 1, 0, 0])
     move_direction2 = np.roll(move_direction1, 2)
@@ -336,3 +337,237 @@ def test_scout_move_entire_row(env_5x5, pices, from_pos):
         validate_move(env, Piece.EMPTY, to_pos, from_pos, maybe_occupied=True)
         assert len(env.two_square_detector.p1) == 1
         assert len(env.two_square_detector.p2) == 1
+
+class TestScoutPositionalMovement:
+
+    def test_range_narrowing_end_pos(self, env_5x5: Callable[[Any], StrategoEnv]):
+        env = env_5x5(SCOUT_ONLY)
+        pos1 = (4, 1)  # starting position
+        pos2 = (4, 3)  # final_position: repeat movement in the same direction (pos3 -> pos4 -> pos2)
+        pos3 = (4, 1)
+        pos4 = (4, 2)  
+        pos5 = (4, 0)  # final_position: one cell to the left of the starting position
+        for final_position in (pos2, pos5):
+            env.reset()
+            repeat_twice(env.step, pos1)
+            piece = Piece(env.board[pos1])
+            assert piece.value > Piece.LAKE.value
+            assert env.game_phase == GamePhase.SELECT
+            repeat_twice(move_fwd, env, pos1, pos2)
+            validate_move(env, piece, pos1, pos2)
+
+            repeat_twice(move_bwd, env, pos3, pos2)
+            validate_move(env, piece, pos2, pos3)
+
+            repeat_twice(move_fwd, env, pos3, pos4)
+            validate_move(env, piece, pos3, pos4)
+
+            assert not env.two_square_detector.validate_move(Player.RED, piece, pos4, pos3)
+            assert not env.two_square_detector.validate_move(Player.BLUE, piece, pos4, pos3)
+            assert len(env.two_square_detector.p1) == 3
+            assert len(env.two_square_detector.p2) == 3
+
+            assert env.two_square_detector.validate_move(Player.RED, piece, pos4, final_position)
+            assert env.two_square_detector.validate_move(Player.BLUE, piece, pos4, final_position)
+            repeat_twice(move_fwd, env, pos4, final_position)
+            validate_move(env, piece, pos4, final_position)
+            assert len(env.two_square_detector.p1) == 1
+            assert len(env.two_square_detector.p2) == 1
+
+    def test_range_narrowing_start_pos(self, env_5x5: Callable[[Any], StrategoEnv]):
+        env = env_5x5(SCOUT_ONLY)
+        pos1 = (4, 1)  # starting position;
+        pos2 = (4, 3)
+        pos3 = (4, 2)
+        pos4 = (4, 3)
+        pos5 = (4, 0)  # final_position: one cell to the left of the starting position
+        pos6 = (4, 4)  # final_position: one cell to the right of the intermediate position (pos2, pos4)
+        for final_position in (pos5, pos6):
+            env.reset()
+            repeat_twice(env.step, pos1)
+            piece = Piece(env.board[pos1])
+            assert piece.value > Piece.LAKE.value
+            assert env.game_phase == GamePhase.SELECT
+            repeat_twice(move_fwd, env, pos1, pos2)
+            validate_move(env, piece, pos1, pos2)
+
+            repeat_twice(move_bwd, env, pos3, pos2)
+            validate_move(env, piece, pos2, pos3)
+
+            repeat_twice(move_fwd, env, pos3, pos4)
+            validate_move(env, piece, pos3, pos4)
+
+            assert not env.two_square_detector.validate_move(Player.RED, piece, pos4, pos3)
+            assert not env.two_square_detector.validate_move(Player.BLUE, piece, pos4, pos3)
+            assert not env.two_square_detector.validate_move(Player.RED, piece, pos4, pos1)
+            assert not env.two_square_detector.validate_move(Player.BLUE, piece, pos4, pos1)
+            assert len(env.two_square_detector.p1) == 3
+            assert len(env.two_square_detector.p2) == 3
+
+            assert env.two_square_detector.validate_move(Player.RED, piece, pos4, final_position)
+            assert env.two_square_detector.validate_move(Player.BLUE, piece, pos4, final_position)
+            repeat_twice(move_fwd, env, pos4, final_position)
+            validate_move(env, piece, pos4, final_position)
+            assert len(env.two_square_detector.p1) == 1
+            assert len(env.two_square_detector.p2) == 1
+
+    def test_range_narrowing_start_end_pos(self, env_5x5: Callable[[Any], StrategoEnv]):
+        env = env_5x5(SCOUT_ONLY)
+        pos1 = (4, 1)  # starting position;
+        pos2 = (4, 4)  # final_position: repeat movement in the same direction (pos3 -> pos4 -> pos2)
+        pos3 = (4, 2)
+        pos4 = (4, 3)
+        pos5 = (4, 0)  # final_position: one cell to the left of the starting position
+        for final_position in (pos2, pos5):
+            env.reset()
+            repeat_twice(env.step, pos1)
+            piece = Piece(env.board[pos1])
+            assert piece.value > Piece.LAKE.value
+            assert env.game_phase == GamePhase.SELECT
+            repeat_twice(move_fwd, env, pos1, pos2)
+            validate_move(env, piece, pos1, pos2)
+
+            repeat_twice(move_bwd, env, pos3, pos2)
+            validate_move(env, piece, pos2, pos3)
+
+            repeat_twice(move_fwd, env, pos3, pos4)
+            validate_move(env, piece, pos3, pos4)
+
+            assert not env.two_square_detector.validate_move(Player.RED, piece, pos4, pos3)
+            assert not env.two_square_detector.validate_move(Player.BLUE, piece, pos4, pos3)
+            assert not env.two_square_detector.validate_move(Player.RED, piece, pos4, pos1)
+            assert not env.two_square_detector.validate_move(Player.BLUE, piece, pos4, pos1)
+            assert len(env.two_square_detector.p1) == 3
+            assert len(env.two_square_detector.p2) == 3
+
+            assert env.two_square_detector.validate_move(Player.RED, piece, pos4, final_position)
+            assert env.two_square_detector.validate_move(Player.BLUE, piece, pos4, final_position)
+            repeat_twice(move_fwd, env, pos4, final_position)
+            validate_move(env, piece, pos4, final_position)
+            assert len(env.two_square_detector.p1) == 1
+            assert len(env.two_square_detector.p2) == 1
+
+    def test_range_extension_end_pos(self, env_5x5: Callable[[Any], StrategoEnv]):
+        env = env_5x5(SCOUT_ONLY)
+        pos1 = (4, 1)  # starting position
+        pos2 = (4, 2)
+        pos3 = (4, 1)
+        pos4 = (4, 3)
+        repeat_twice(env.step, pos1)
+        piece = Piece(env.board[pos1])
+        assert piece.value > Piece.LAKE.value
+        assert env.game_phase == GamePhase.SELECT
+        repeat_twice(move_fwd, env, pos1, pos2)
+        validate_move(env, piece, pos1, pos2)
+
+        repeat_twice(move_bwd, env, pos3, pos2)
+        validate_move(env, piece, pos2, pos3)
+
+        repeat_twice(move_fwd, env, pos3, pos4)
+        validate_move(env, piece, pos3, pos4)
+
+        assert env.two_square_detector.validate_move(Player.RED, piece, pos4, pos3)
+        assert env.two_square_detector.validate_move(Player.BLUE, piece, pos4, pos3)
+        repeat_twice(move_fwd, env, pos4, pos3)
+        validate_move(env, piece, pos4, pos3)
+        assert len(env.two_square_detector.p1) == 2  # (4, 1) -> (4, 3) -> (4, 1)
+        assert len(env.two_square_detector.p2) == 2
+
+    def test_range_extension_start_pos(self, env_5x5: Callable[[Any], StrategoEnv]):
+        env = env_5x5(SCOUT_ONLY)
+        pos1 = (4, 1)  # starting position
+        pos2 = (4, 3)
+        pos3 = (4, 0)
+        pos4 = (4, 3)
+        repeat_twice(env.step, pos1)
+        piece = Piece(env.board[pos1])
+        assert piece.value > Piece.LAKE.value
+        assert env.game_phase == GamePhase.SELECT
+        repeat_twice(move_fwd, env, pos1, pos2)
+        validate_move(env, piece, pos1, pos2)
+
+        repeat_twice(move_bwd, env, pos3, pos2)
+        validate_move(env, piece, pos2, pos3)
+
+        repeat_twice(move_fwd, env, pos3, pos4)
+        validate_move(env, piece, pos3, pos4)
+
+        assert env.two_square_detector.validate_move(Player.RED, piece, pos4, pos3)
+        assert env.two_square_detector.validate_move(Player.BLUE, piece, pos4, pos3)
+        repeat_twice(move_fwd, env, pos4, pos3)
+        validate_move(env, piece, pos4, pos3)
+        assert len(env.two_square_detector.p1) == 3  # (4, 3) -> (4, 0) -> (4, 3) -> (4, 0)
+        assert len(env.two_square_detector.p2) == 3
+
+    def test_range_extension_start_end_pos(self, env_5x5: Callable[[Any], StrategoEnv]):
+        env = env_5x5(SCOUT_ONLY)
+        pos1 = (4, 1)  # starting position
+        pos2 = (4, 2)
+        pos3 = (4, 0)
+        pos4 = (4, 3)
+        repeat_twice(env.step, pos1)
+        piece = Piece(env.board[pos1])
+        assert piece.value > Piece.LAKE.value
+        assert env.game_phase == GamePhase.SELECT
+        repeat_twice(move_fwd, env, pos1, pos2)
+        validate_move(env, piece, pos1, pos2)
+
+        repeat_twice(move_bwd, env, pos3, pos2)
+        validate_move(env, piece, pos2, pos3)
+
+        repeat_twice(move_fwd, env, pos3, pos4)
+        validate_move(env, piece, pos3, pos4)
+
+        assert env.two_square_detector.validate_move(Player.RED, piece, pos4, pos3)
+        assert env.two_square_detector.validate_move(Player.BLUE, piece, pos4, pos3)
+        repeat_twice(move_fwd, env, pos4, pos3)
+        validate_move(env, piece, pos4, pos3)
+        assert len(env.two_square_detector.p1) == 2  # (4, 0) -> (4, 3) -> (4, 0)
+        assert len(env.two_square_detector.p2) == 2
+
+    def test_movement_sample(self, env_10x10: Callable[[Any], StrategoEnv]):
+        env = env_10x10(SCOUT_ONLY)
+        from_pos = (6, 1)
+        repeat_twice(env.step, from_pos)
+        piece = Piece(env.board[from_pos])
+        assert piece.value > Piece.LAKE.value
+        assert env.game_phase == GamePhase.SELECT
+
+        event_list = [
+            (False, (6, 6)),
+            (False, (6, 1)),
+            (False, (6, 6)),
+            (True, (6, 1)),  # equal jumps
+            (False, (6, 0)),
+            (False, (6, 6)),
+            (False, (6, 4)),
+            (True, (6, 6)),  # narrowing start pos
+            (False, (6, 7)),
+            (False, (6, 4)),
+            (False, (6, 6)),
+            (True, (6, 4)),  # narrowing end pos
+            (False, (6, 3)),
+            (False, (6, 6)),
+            (False, (6, 4)),
+            (True, (6, 6)),  # narrowing start pos
+            (False, (6, 7)),
+            (False, (6, 2)),
+            (False, (6, 4)),
+            (False, (6, 0)),
+            (False, (6, 6)),
+            (False, (6, 4)),
+            (False, (6, 7)),
+            (False, (6, 4)),
+            (False, (6, 6)),
+            (True, (6, 4)),  # narrowing end pos
+            (False, (6, 3)),
+        ]
+
+        for need_check, to_pos in event_list:
+            if not need_check:
+                repeat_twice(move_fwd, env, from_pos, to_pos)
+                validate_move(env, piece, from_pos, to_pos)
+                from_pos = to_pos
+            else:
+                assert not env.two_square_detector.validate_move(Player.RED, piece, from_pos, to_pos)
+                assert not env.two_square_detector.validate_move(Player.BLUE, piece, from_pos, to_pos)
