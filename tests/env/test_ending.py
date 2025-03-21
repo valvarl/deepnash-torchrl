d9e11a6be60604371707d9dfa800f14d3081a9c7
Valery Varlachev <valvarl@ya.ru>
2025-03-21 19:10:13 +0300

    pieces stucked and no pieces left

--- tests/env/test_ending.py
+++ tests/env/test_ending.py
@@ -7,7 +7,8 @@ import pytest
 from stratego_gym.envs.primitives import Piece, Player, Pos
 from stratego_gym.envs.startego import GamePhase, StrategoEnv
 from tests.env.utils import (
-    SCOUT_ONLY, SPY_ONLY, FLAG_2BOMB_SCOUT, FLAG_2BOMB_SPY,
+    FLAG_2BOMB_SCOUT, FLAG_2BOMB_SPY,
+    SCOUT_ONLY, SPY_ONLY, SCOUT_PAIR, SPY_PAIR,
     move_fwd, repeat_twice, rotate_pos, validate_move
 )
 
@@ -205,3 +206,179 @@ def test_two_square_rule_piece_stucked(env_5x5, pieces, red_stucked, blue_stucke
     else:
         # not red_stucked and not blue_stucked
         pass
+
+@pytest.mark.parametrize(
+    "pieces,red_stucked,blue_stucked",
+    itertools.product(
+        [SCOUT_PAIR, SPY_PAIR],
+        [True, False],
+        [True, False],
+    )
+)
+def test_two_square_rule_two_pieces_stucked(env_5x5, pieces, red_stucked, blue_stucked):
+    """
+    B.sL.
+    .bb..
+    .....
+    ..bb.
+    .Ls.R
+    """
+    lakes_mask = np.zeros((5, 5), dtype=bool)
+    lakes_mask[4, 1] = True
+    lakes_mask[0, 3] = True
+    p1_deploy_mask = np.zeros((5, 5), dtype=bool)
+    p2_deploy_mask = np.zeros((5, 5), dtype=bool)
+    p1_deploy_mask[3:] = True
+    p2_deploy_mask[:2] = True
+    p1_deploy_mask &= ~lakes_mask
+    p2_deploy_mask &= ~lakes_mask
+
+    _pieces = {Piece.BOMB: 2}
+    _pieces.update(pieces)
+    env = env_5x5(_pieces, lakes_mask=lakes_mask, p1_deploy_mask=p1_deploy_mask, p2_deploy_mask=p2_deploy_mask)
+    deploy_pos = [(3, 2), (3, 3), (4, 2), (4, 4)]
+    for from_pos in deploy_pos:
+        repeat_twice(env.step, from_pos)
+
+    assert env.game_phase == GamePhase.SELECT
+    stuck_direction = np.array([-1, 1, -1])
+    from_pos_red = from_pos
+    from_pos_blue = from_pos
+    for i, direction in enumerate(stuck_direction):
+        if red_stucked:
+            to_pos_red = (from_pos_red[0], from_pos_red[1] + direction)
+        else:
+            to_pos_red = (from_pos_red[0] - 1, from_pos_red[1])
+        state, reward, terminated, truncated, info = env.step(from_pos_red)
+        state, reward, terminated, truncated, info = env.step(to_pos_red)
+        from_pos_red = to_pos_red
+
+        if blue_stucked:
+            to_pos_blue = (from_pos_blue[0], from_pos_blue[1] + direction)
+        else:
+            to_pos_blue = (from_pos_blue[0] - 1, from_pos_blue[1])
+        state, reward, terminated, truncated, info = env.step(from_pos_blue)
+        state, reward, terminated, truncated, info = env.step(to_pos_blue)
+        from_pos_blue = to_pos_blue
+
+    if red_stucked and blue_stucked or red_stucked:
+        assert env.game_phase == GamePhase.TERMINAL
+        # If the game ends with blue's turn, but he can move, he is awarded 1 on his turn.
+        assert (reward == 0) if blue_stucked else (reward == 1)
+    elif blue_stucked:
+        assert env.game_phase == GamePhase.SELECT
+        state, reward, terminated, truncated, info = env.step(env.action_space.sample())
+        state, reward, terminated, truncated, info = env.step(env.action_space.sample())
+        assert env.game_phase == GamePhase.TERMINAL
+        assert reward == 1
+    else:
+        # not red_stucked and not blue_stucked
+        pass
+
+@pytest.mark.parametrize(
+    "pieces,red_no_pieces,blue_no_pieces",
+    itertools.product(
+        [SCOUT_PAIR, SPY_PAIR],
+        [True, False],
+        [True, False],
+    )
+)
+def test_no_pieces_left(env_5x5, pieces, red_no_pieces, blue_no_pieces):
+    """
+    sb..B
+    b....
+    ..L..
+    ....b
+    R..bs
+    """
+    _pieces = {Piece.BOMB: 2}
+    _pieces.update(pieces)
+    env = env_5x5(_pieces)
+    deploy_pos = [(3, 4), (4, 3), (4, 4)]
+    for from_pos in deploy_pos:
+        repeat_twice(env.step, from_pos)
+
+    from_pos_red = (4, 0)
+    if not red_no_pieces:
+        from_pos_red = (4, 1)
+    from_pos_blue = (4, 0)
+    if not blue_no_pieces:
+        from_pos_blue = (4, 1)
+
+    env.step(from_pos_red)
+    env.step(from_pos_blue)
+
+    for i in range(3):
+        to_pos_red = (from_pos_red[0] - 1, from_pos_red[1])
+        state, reward, terminated, truncated, info = env.step(from_pos_red)
+        state, reward, terminated, truncated, info = env.step(to_pos_red)
+        from_pos_red = to_pos_red
+
+        to_pos_blue = (from_pos_blue[0] - 1, from_pos_blue[1])
+        state, reward, terminated, truncated, info = env.step(from_pos_blue)
+        state, reward, terminated, truncated, info = env.step(to_pos_blue)
+        from_pos_blue = to_pos_blue
+
+    if red_no_pieces and blue_no_pieces or red_no_pieces:
+        assert env.game_phase == GamePhase.TERMINAL
+        # If the game ends with blue's turn, but he can move, he is awarded 1 on his turn.
+        assert (reward == 0) if blue_no_pieces else (reward == 1)
+    elif blue_no_pieces:
+        assert env.game_phase == GamePhase.SELECT
+        to_pos_red = (from_pos_red[0], from_pos_red[1] + 1)
+        state, reward, terminated, truncated, info = env.step(from_pos_red)
+        state, reward, terminated, truncated, info = env.step(to_pos_red)
+        assert env.game_phase == GamePhase.TERMINAL
+        assert reward == 1
+    else:
+        # not red_no_pieces and not blue_no_pieces
+        pass
+
+@pytest.mark.parametrize(
+    "pieces,red_attacker",
+    itertools.product(
+        [SCOUT_PAIR, SPY_PAIR],
+        [True, False],
+    )
+)
+def test_no_pieces_left_on_trade(env_5x5, pieces, red_attacker):
+    """
+    sbB..
+    b....
+    .....
+    ....b
+    ..Rbs
+    """
+    _pieces = {Piece.BOMB: 2}
+    _pieces.update(pieces)
+    env = env_5x5(_pieces, lakes=[])
+    deploy_pos = [(3, 4), (4, 3), (4, 4)]
+    for from_pos in deploy_pos:
+        repeat_twice(env.step, from_pos)
+
+    from_pos_red = (3, 2)
+    if not red_attacker:
+        from_pos_red = (4, 2)
+    from_pos_blue = (4, 2)
+
+    env.step(from_pos_red)
+    env.step(from_pos_blue)
+
+    assert env.game_phase == GamePhase.SELECT
+    for i in range(2):
+        to_pos_red = (from_pos_red[0] - 1, from_pos_red[1])
+        state, reward, terminated, truncated, info = env.step(from_pos_red)
+        state, reward, terminated, truncated, info = env.step(to_pos_red)
+        from_pos_red = to_pos_red
+
+        if i == 1 and red_attacker:
+            break
+
+        to_pos_blue = (from_pos_blue[0] - 1, from_pos_blue[1])
+        state, reward, terminated, truncated, info = env.step(from_pos_blue)
+        state, reward, terminated, truncated, info = env.step(to_pos_blue)
+        from_pos_blue = to_pos_blue
+
+    assert env.game_phase == GamePhase.TERMINAL
+    assert reward == 0
+    
