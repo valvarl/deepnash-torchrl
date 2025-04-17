f6ca158b9c03a9ed490c057819f624e6ca06a4a3
Varlachev Valery <valvarl@ya.ru>
2025-04-17 16:50:26 +0300

    update hydra config

--- test_hydra.py
+++ test_hydra.py
@@ -2,11 +2,11 @@ import hydra
 from omegaconf import DictConfig, OmegaConf
 
 
-@hydra.main(version_base=None, config_path="conf", config_name="config")
+@hydra.main(version_base="1.3", config_path="conf", config_name="config")
 def main(cfg: DictConfig) -> None:
     print(OmegaConf.to_yaml(cfg))
-    print("Actor GPUs:", cfg.deepnash.learn.deployment_config.actor_gpu_count)
-    print("Mode:", cfg.deepnash.learn.deployment_config.mode)
+    print("Actor GPUs:", cfg.deepnash.training.deployment_config.actor_gpu_count)
+    print("Mode:", cfg.deepnash.training.deployment_config.mode)
 
 
 if __name__ == "__main__":
